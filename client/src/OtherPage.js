70a680f8c54b7cab69c8b48b6ac718d18fa30367
Utkarsh Shukla <utkarsh.shukla@hitachids.com>
2026-06-22 16:27:56 +0530

    initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () =>{
+    return (
+        <div>
+            I'm the other page. Go back to <Link to="/">Home</Link>
+        </div>
+);};
